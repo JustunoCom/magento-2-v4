0ef3ceb19c6efae41d54a65f2745275268fce8b5
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 03:27:41 +0000

    https://github.com/justuno-com/core/issues/374

--- Settings.php
+++ Settings.php
@@ -20,16 +20,14 @@ final class Settings extends \Justuno\Core\Config\Settings {
 	/**
 	 * 2022-07-13 "Implement the «Custom Subdomain» field": https://github.com/JustunoCom/magento-2-v4/issues/2
 	 * @used-by \Justuno\M2\Block\Js::_toHtml()
-	 * @return string
 	 */
-	function domain() {return $this->v();}
+	function domain():string {return $this->v();}
 
 	/**
 	 * 2021-03-06
 	 * @override
 	 * @see \Justuno\Core\Config\Settings::prefix()
 	 * @used-by \Justuno\Core\Config\Settings::v()
-	 * @return string
 	 */
-	protected function prefix() {return 'justuno_settings/options_interface';}
+	protected function prefix():string {return 'justuno_settings/options_interface';}
 }
