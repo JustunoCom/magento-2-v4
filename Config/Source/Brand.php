d4c4099609d535631853f49d362c694520685f42
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 03:03:11 +0000

    https://github.com/justuno-com/core/issues/374

--- Config/Source/Brand.php
+++ Config/Source/Brand.php
@@ -12,7 +12,7 @@ final class Brand extends \Justuno\Core\Config\Source {
 	protected function map():array {return ju_map_0(array_column(
 		array_filter(
 			ju_fetch('eav_attribute', ['attribute_code', 'frontend_label', 'is_user_defined'], 'entity_type_id', 4)
-			,function(array $a) {return
+			,function(array $a):bool {return
 				/**
 				 * 2019-10-25 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
 				 * "The `manufaturer` attribute is absent in the «Choose Brand Attribute» backend dropdown":
