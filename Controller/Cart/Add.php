26c707261e68ee87d5176ce158c6f53c9eff048c
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 03:06:46 +0000

    https://github.com/justuno-com/core/issues/374

--- Controller/Cart/Add.php
+++ Controller/Cart/Add.php
@@ -16,9 +16,8 @@ class Add extends _P {
 	 * @used-by \Magento\Framework\App\Action\Action::dispatch():
 	 * 		$result = $this->execute();
 	 * https://github.com/magento/magento2/blob/2.2.1/lib/internal/Magento/Framework/App/Action/Action.php#L84-L125
-	 * @return Json
 	 */
-	function execute() {return R::p(function() {
+	function execute():Json {return R::p(function() {
 		/**
 		 * 2020-01-21
 		 * @see \Magento\Checkout\Controller\Cart\Add::_initProduct()
